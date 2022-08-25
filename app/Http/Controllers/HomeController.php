909179ca1d67ac91379d874e40ac851edb82a2cb
Leandro Leiva <leandro@innovit.cl>
2022-08-24 22:51:08 -0400

    add try catch with error message

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,13 +29,18 @@ class HomeController extends Controller
 
     public function dolarPost(Request $request)
     {
-        $validatedData = $request->validate([
-            'date' => 'required|date'
-        ]);
-        $date = date('Y-m-d',strtotime($request->date));
-        $m = date('m',strtotime($date));
-        $y = date('Y',strtotime($date));
-        $items = collect(json_decode(file_get_contents('https://api.sbif.cl/api-sbifv3/recursos_api/dolar/'.$y.'/'.$m.'?apikey=d8093171162117c0c6e8da895b00978d4e2b6a0e&formato=json')));
+        try {
+            $validatedData = $request->validate([
+                'date' => 'required|date'
+            ]);
+            $date = date('Y-m-d',strtotime($request->date));
+            $m = date('m',strtotime($date));
+            $y = date('Y',strtotime($date));
+            $items = collect(json_decode(file_get_contents('https://api.sbif.cl/api-sbifv3/recursos_api/dolar/'.$y.'/'.$m.'?apikey=d8093171162117c0c6e8da895b00978d4e2b6a0e&formato=json')));
+        } catch (\Throwable $th) {
+            return view('home',compact('date'))
+                    ->withErrors(["Error"=>"No se pudo realizar la petición, intentelo más tarde"]);
+        }
         return view('home',compact('date','items'));
     }
 }
