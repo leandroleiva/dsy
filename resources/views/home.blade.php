93a23ff691072c2a4320f2210331480356fe96f0
Leandro Leiva <leandro@innovit.cl>
2022-08-27 01:42:21 -0400

    fix charts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -77,6 +77,7 @@
     @endif
 </div>
 <script>
+    @if(isset($items))
     const ctx = document.getElementById('myChart').getContext('2d');
     const myChart = new Chart(ctx, {
         type: 'line',
@@ -99,6 +100,7 @@
             }]
         }
     });
+    @endif
     </script>
     
 @endsection
